391205a04b6f964262e86036bdbc6eb9734fcd16
unknown <abdeladim.laaroua@gmail.com>
2017-12-11 23:54:09 +0000

    add other files to reports

---
+++ application/views/admin/daily_report/send.php
@@ -0,0 +1,8 @@
+<?php $this->load->view('admin/partials/admin_header.php'); ?>
+	<!-- page content -->
+	<div class="right_col" role="main">
+		<b>MESSAGE SENT : <?php echo $message ?></b> 
+	</div>
+
+	<!-- /page content -->
+<?php $this->load->view('admin/partials/admin_footer'); ?>
