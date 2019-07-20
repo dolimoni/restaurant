db3e93ced95d5bc83e2da2eadf46f313e64e4222
essalhi <khalid.essalhi8@gmail.com>
2019-07-20 01:21:00 +0100

    stockitmain

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'restaurant';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
@@ -80,6 +80,24 @@ $db['remote_2']['dbcollat'] = 'utf8_general_ci';
 $db['remote_2']['swap_pre'] = '';
 $db['remote_2']['autoinit'] = TRUE;
 $db['remote_2']['stricton'] = FALSE;
+
+
+$db['stockitmain']['hostname'] = 'localhost';//localhost ou 149.91.80.68
+$db['stockitmain']['username'] = 'root';//root ou lhpastry
+$db['stockitmain']['password'] = 'root'; // ou lhpastry
+$db['stockitmain']['database'] = 'stockitmain';//restaurant_tmp ou restaurant_test
+$db['stockitmain']['dbdriver'] = 'mysqli';
+$db['stockitmain']['dbprefix'] = '';
+$db['stockitmain']['pconnect'] = TRUE;
+$db['stockitmain']['db_debug'] = TRUE;
+$db['stockitmain']['cache_on'] = FALSE;
+$db['stockitmain']['cachedir'] = '';
+$db['stockitmain']['char_set'] = 'utf8';
+$db['stockitmain']['dbcollat'] = 'utf8_general_ci';
+$db['stockitmain']['swap_pre'] = '';
+$db['stockitmain']['autoinit'] = TRUE;
+$db['stockitmain']['stricton'] = FALSE;
+
 
 
 
